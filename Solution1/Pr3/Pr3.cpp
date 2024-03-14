2ade1d73b0afd3fe8d171f817338b8184a00bdd8
Yaroslav <yarsolavsokol55@gmail.com>
2024-03-14 15:41:47 +0200

    Complete

---
+++ Solution1/Pr3/Pr3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[10];
+	int number;
+
+
+	cout << "Enter 10 elements: " << endl;
+	for (int i = 0; i < size(arr); i++) {
+		cout << i + 1 << ". ";
+		cin >> arr[i];
+	}
+
+	bool flag = true;
+	int count = 0;
+
+	while (flag = true) {
+
+		cout << "Which number you want search: " << endl;
+		cin >> number;
+
+		for (int i = 0; i < size(arr); i++) {
+			if (number == arr[i]) {
+				cout << "Number placed on " << i + 1 << " position" << endl;
+				flag = false;
+				count++;
+				return 0;
+
+
+			}
+
+
+		}
+		if (count == 0) {
+			cout << "Number not found! ERORR 404 :(";
+		}
+	}
+
+}
